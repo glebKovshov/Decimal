92885e0374a6ab60e19a9155bce651fb01ddf2ca
KovshovPC <kovshov.gleb@gmail.com>
2024-10-27 21:06:45 +0300

    comparison operators example

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@
 int main() {
 	auto start = std::chrono::high_resolution_clock::now();
 
-	Decimal num1 = "108746234632.3981723", num2 = "102131.9317293713";
+	Decimal num1 = "-11011.63";
+	Decimal num2 = "-11211.9831";
 
-	Decimal num3 = num1 + num2;
-	std::cout << num1 << " + " << num2 << " = " << num3 << std::endl;
-
-	std::cout << num1 << " > " << num2 << " = " << std::boolalpha << (num1 > num2) << std::endl;
+	std::cout << num1 <<" > " << num2 << " " << std::boolalpha << (num1 > num2) << std::endl;
 
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> duration = end - start;
 	std::cout << "Executing time: " << duration.count() << " sec" << std::endl;
+	
 }
