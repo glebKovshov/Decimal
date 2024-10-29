0d72e96fbf5867c47f1bcce9b88877f555fa40c8
KovshovPC <kovshov.gleb@gmail.com>
2024-10-29 13:08:09 +0300

    Completed operator '+'

--- Decimal.cpp
+++ Decimal.cpp
@@ -82,107 +82,129 @@ Decimal Decimal::abs(Decimal& other) noexcept {
 	return Decimal(result);
 }
 
-Decimal Decimal::operator+(Decimal& other) noexcept {
+Decimal Decimal::operator +(Decimal& other) noexcept {
+	if (this->_num[0] == '-' && other._num[0] != '-') {      // -a + b = b - a
+		Decimal absthis = Decimal::abs(*this);
+		return other - absthis;
+	}
+	else if (this->_num[0] != '-' && other._num[0] == '-') { // a + (-b) = a - b
+		Decimal absother = Decimal::abs(other);
+		return *this - absother;
+	}
+	else if (this->_num[0] == '-' && other._num[0] == '-') { // -a + (-b) = -a - b = -(a+b)
+		
+		Decimal absthis = Decimal::abs(*this);
+		Decimal absother = Decimal::abs(other);
+		Decimal result = absthis + absother;
 
-	std::vector<char> fracpart;
-	std::vector<char> intpart;
-	UInt16 carryover = 0;
-	UInt64 fraclen1 = 0;
-	UInt64 fraclen2 = 0;
-	UInt64 start1 = 0;
-	UInt64 start2 = 0;
-	Int64 end1 = 0;
-	Int64 end2 = 0;
-	UInt16 result = 0;
-	UInt64 index = 0;
-	Int64 dotpos1 = this->find('.');
-	Int64 dotpos2 = other.find('.');
-	Int64 i = 0;
+		char* num = new char[*result._size + 1];
+		num[0] = '-';
 
-	if (dotpos1 != -1) fraclen1 = *this->_size - dotpos1 - 1; //Checking float numbers for fraction part length
-	if (dotpos2 != -1) fraclen2 = *other._size - dotpos2 - 1;
+		for (UInt64 i = 0; i <= *result._size; i++) num[i + 1] = result._num[i];
+		
+		return Decimal(num);
+	}
+	else {													// a + b
+		std::vector<char> fracpart;
+		std::vector<char> intpart;
+		UInt16 carryover = 0;
+		UInt64 fraclen1 = 0;
+		UInt64 fraclen2 = 0;
+		UInt64 start1 = 0;
+		UInt64 start2 = 0;
+		Int64 end1 = 0;
+		Int64 end2 = 0;
+		UInt16 result = 0;
+		UInt64 index = 0;
+		Int64 dotpos1 = this->find('.');
+		Int64 dotpos2 = other.find('.');
+		Int64 i = 0;
 
-	if (fraclen1 > fraclen2) {
-		for (i = *this->_size - 1; i > dotpos1 + fraclen2; i--) fracpart.push_back(this->_num[i]);
-		start1 = *this->_size - (fraclen1 - fraclen2) - 1;
-		end1 = dotpos1;
-		if (dotpos2 == -1) {
-			end2 = *other._size - 1;
+		if (dotpos1 != -1) fraclen1 = *this->_size - dotpos1 - 1; //Checking float numbers for fraction part length
+		if (dotpos2 != -1) fraclen2 = *other._size - dotpos2 - 1;
+
+		if (fraclen1 > fraclen2) {
+			for (i = *this->_size - 1; i > dotpos1 + fraclen2; i--) fracpart.push_back(this->_num[i]);
+			start1 = *this->_size - (fraclen1 - fraclen2) - 1;
+			end1 = dotpos1;
+			if (dotpos2 == -1) {
+				end2 = *other._size - 1;
+			}
+			else {
+				start2 = *other._size - 1;
+				end2 = dotpos2;
+			}
 		}
-		else {
+
+		else if (fraclen1 < fraclen2) {
+			for (i = *other._size - 1; i > dotpos2 + fraclen1; i--) fracpart.push_back(other._num[i]);
+			start2 = *other._size - (fraclen2 - fraclen1) - 1;
+			end2 = dotpos2;
+			if (dotpos1 == -1) {
+				end1 = *this->_size - 1;
+			}
+			else {
+				start1 = *this->_size - 1;
+				end1 = dotpos1;
+			}
+		}
+
+		else if (fraclen1 == fraclen2 && fraclen1 != 0) {
+			start1 = *this->_size - 1;
+			end1 = dotpos1;
 			start2 = *other._size - 1;
 			end2 = dotpos2;
 		}
-	}
 
-	else if (fraclen1 < fraclen2) {
-		for (i = *other._size - 1; i > dotpos2 + fraclen1; i--) fracpart.push_back(other._num[i]);
-		start2 = *other._size - (fraclen2 - fraclen1) - 1;
-		end2 = dotpos2;
-		if (dotpos1 == -1) {
-			end1 = *this->_size - 1;
-		}
 		else {
-			start1 = *this->_size - 1;
-			end1 = dotpos1;
+			end1 = *this->_size - 1;
+			end2 = *other._size - 1;
 		}
-	}
 
-	else if (fraclen1 == fraclen2 && fraclen1 != 0) {
-		start1 = *this->_size - 1;
-		end1 = dotpos1;
-		start2 = *other._size - 1;
-		end2 = dotpos2;
-	}
-
-	else {
-		end1 = *this->_size - 1;
-		end2 = *other._size - 1;
-	}
-
-	while (start1 > end1 && start1 > 0 && start2 > end2 && start2 > 0) {
-		result = CharToDigit(this->_num[start1]) + CharToDigit(other._num[start2]) + carryover;
-		carryover = result / 10;
-		fracpart.push_back(DigitToChar(result % 10));
-		start1--;
-		start2--;
-	}
-
-	if (start1 > 0) end1--;  //Pass dot char
-	if (start2 > 0) end2--;
-
-	while (end1 > -1 || end2 > -1 || carryover > 0) { // Add operation
-		result = 0;
-		if (end1 > -1) {
-			result += CharToDigit(this->_num[end1]);
-			end1--;
+		while (start1 > end1 && start1 > 0 && start2 > end2 && start2 > 0) {
+			result = CharToDigit(this->_num[start1]) + CharToDigit(other._num[start2]) + carryover;
+			carryover = result / 10;
+			fracpart.push_back(DigitToChar(result % 10));
+			start1--;
+			start2--;
 		}
-		if (end2 > -1) {
-			result += CharToDigit(other._num[end2]);
-			end2--;
+
+		if (start1 > 0) end1--;  //Pass dot char
+		if (start2 > 0) end2--;
+
+		while (end1 > -1 || end2 > -1 || carryover > 0) { // Add operation
+			result = 0;
+			if (end1 > -1) {
+				result += CharToDigit(this->_num[end1]);
+				end1--;
+			}
+			if (end2 > -1) {
+				result += CharToDigit(other._num[end2]);
+				end2--;
+			}
+			if (carryover > 0) result += carryover;
+
+			carryover = result / 10;
+			intpart.push_back(DigitToChar(result % 10));
 		}
-		if (carryover > 0) result += carryover;
 
-		carryover = result / 10;
-		intpart.push_back(DigitToChar(result % 10));
+		UInt64 size = intpart.size() + fracpart.size() + 1; // 1 for '\0'
+
+		if (fracpart.size() > 0) size++; // for '.'
+		char* num = new char[size];
+
+		for (i = intpart.size() - 1; i > -1; i--, index++) num[index] = intpart[i];
+
+		if (fracpart.size() > 0) {
+			num[index] = '.';
+			index++;
+			for (i = fracpart.size() - 1; i > -1; i--, index++) num[index] = fracpart[i];
+		}
+
+		num[index] = '\0';
+
+		return Decimal(num);
 	}
-
-	UInt64 size = intpart.size() + fracpart.size() + 1; // 1 for '\0'
-
-	if (fracpart.size() > 0) size++; // for '.'
-	char* num = new char[size];
-
-	for (i = intpart.size() - 1; i > -1; i--, index++) num[index] = intpart[i];
-
-	if (fracpart.size() > 0) {
-		num[index] = '.';
-		index++;
-		for (i = fracpart.size() - 1; i > -1; i--, index++) num[index] = fracpart[i];
-	}
-
-	num[index] = '\0';
-
-	return Decimal(num);
 }
 
 Decimal Decimal::operator - (Decimal& other) noexcept {
@@ -190,7 +212,7 @@ Decimal Decimal::operator - (Decimal& other) noexcept {
 	Decimal absother = Decimal::abs(other);
 
 	if (other._num[0] == '-' && this->_num[0] != '-') { // a - (-b) = a + b
-		return this->Decimal::operator+(absother);
+		return *this + absother;
 	}
 
 	else if (this->_num[0] == '-' && other._num[0] != '-') { // -a - b = -(a+b)
